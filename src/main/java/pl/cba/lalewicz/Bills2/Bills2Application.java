8a68fe29ba505d381885e93c41db73bdbe218752
tomaszlal <lalewicz.tomasz@gmail.com>
2023-01-10 23:49:39 +0100

    second commit

---
+++ src/main/java/pl/cba/lalewicz/Bills2/Bills2Application.java
@@ -0,0 +1,13 @@
+package pl.cba.lalewicz.Bills2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bills2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bills2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/cba/lalewicz/Bills2/Bills2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cba.lalewicz.Bills2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bills2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
